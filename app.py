c40bdcd912fc72458463eac603398c061bf3666c
vanamala shivaji ganesh <67234017+vanamala-19@users.noreply.github.com>
2021-10-05 21:10:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+mail = Mail(app)
+app.config[MAIL_SERVER] = 'smtp.gmail.com'
+app.config[MAIL_PORT] = '465'
+app.config[MAIL_USE_SSL] = 'True'
+app.config[MAIL_USERNAME] =  "187r1a0354@gmail.com"
+app.config[MAIL_PASSWORD] = "yeshudarling"
+
+
 @app.route("/")
 def open():
     return render_template('index.html')
@@ -16,5 +24,16 @@ def open():
 # def calculate():
 #     return redirect("/#Results")
 
+@app.route("/contact", methods = ['GET', 'POST'])
+def  contact():
+    if(request.method == 'POST'):
+        name = request.form['name']
+        email = request.form['email']
+        subject = request.form['subject']
+        msg = request.form['message']
+        mail.send_message('New Message From ' + name,sender=email,recipients="vanamalashivaji19@gmail.com",body=msg)
+        sucess = "Message sent"
+    return  render_template('index.html', sucess=sucess)
+
 if __name__ == "__main__":
-    app.run(debug=True,port=8000)
+    app.run(debug=True)
